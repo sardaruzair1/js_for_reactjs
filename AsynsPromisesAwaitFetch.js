d587bb6c2c772cbd373190cd1087421c34e6cee1
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-04-25 23:16:40 +0500

    asyc

--- AsynsPromisesAwaitFetch.js
+++ AsynsPromisesAwaitFetch.js
@@ -1,2 +1,6 @@
 // async + await + promises + fetch the most important topic of js to learn react and much more libraries of js
 
+async function myFunction() {
+    return "Hello";
+}
+myFunction();
